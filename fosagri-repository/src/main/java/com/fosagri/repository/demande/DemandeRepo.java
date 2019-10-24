5cd76c32c6c2736b36b8503b9ea9df9d57260ba1
fosagrisi <fosagrisi@gmail.com>
2019-10-24 18:05:01 +0100

    Branch to be deployed in FOS-AGri Servers

--- fosagri-repository/src/main/java/com/fosagri/repository/demande/DemandeRepo.java
+++ fosagri-repository/src/main/java/com/fosagri/repository/demande/DemandeRepo.java
@@ -3,12 +3,15 @@ package com.fosagri.repository.demande;
 import com.fosagri.model.entity.Demande;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.rest.core.annotation.RestResource;
 
 import java.util.List;
 
-@RestResource(path = "demandes", rel = "demandes")
-  public interface DemandeRepository extends JpaRepository<Demande, Long>
-{
-  	List<Demande> findAllBy(Pageable pageable);
+
+
+public interface DemandeRepo extends CrudRepository<Demande, Long> {
+
+
 }
--- fosagri-rest/src/main/java/com/fosagri/rest/DemandeApi.java
+++ fosagri-rest/src/main/java/com/fosagri/rest/DemandeApi.java
@@ -1,11 +1,33 @@
 package com.fosagri.rest;
 
 
+import com.fosagri.model.entity.Demande;
+import com.fosagri.repository.demande.DemandeRepo;
+import com.fosagri.service.demande.DemandeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
- public class DemandeApi {
+@RequestMapping("/api")
+public class DemandeApi {
+ @Autowired
+ DemandeService demandeRepo;
 
+ @GetMapping("/demandes")
+ public List<Demande> getAllDemandes() {
+
+  System.out.println("Get all AdhAgents...");
+  List<Demande> list = new ArrayList<>();
+  Iterable<Demande> customers = demandeRepo.findAllDemandes();
+  customers.forEach(list::add);
+  System.out.println(list.size());
+
+  return list;
+ }
 
 }
